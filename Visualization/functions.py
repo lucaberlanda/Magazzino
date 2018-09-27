8e85091dcf6f1c5c2e616b259f14834bedf1766a
Luca Berlanda <luca.berlanda@euclidea.com>
2018-09-27 14:48:15 +0200

    various

--- Visualization/functions.py
+++ Visualization/functions.py
@@ -6,15 +6,19 @@ import seaborn as sns
 sns.set_style('white')
 
 
-def quartiles():
+def quartiles(length=2000, n_timeseries=100, save_figure=False, threshold_percentage=0.75):
 
-    prices = pd.DataFrame(np.random.randn(2000, 100) / 100 + 1).cumprod()
+    prices = pd.DataFrame(np.random.randn(length, n_timeseries) / 100 + 1).cumprod()
     prices = prices / prices.loc[0, :]  # rebase prices
     sorted_prices = prices.sort_values(prices.index.values[-1], axis=1)
 
-    plt.figure(1, figsize=(13, 10))
+    threshold = round(n_timeseries * threshold_percentage)
+
+    plt.figure(1, figsize=(11, 6))
     plt.subplot(111)
-    plt.plot(sorted_prices.iloc[:, :66], c='gray')
-    plt.plot(sorted_prices.iloc[:, 66:], c='blue')
-    # plt.show()
-    plt.savefig('demo.png', transparent=True)
+    plt.plot(sorted_prices.iloc[:, :threshold], c='black', linewidth=0.8)
+    plt.plot(sorted_prices.iloc[:, threshold:], c='teal', linewidth=0.8)
+    if save_figure:
+        plt.savefig('demo2.png', transparent=True)
+    else:
+        plt.show()
