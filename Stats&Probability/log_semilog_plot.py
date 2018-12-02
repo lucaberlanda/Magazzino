5f237c3f0839cba02b0301dc32987d3070812d8a
BabyBetulla <luca.berlanda@gmail.com>
2018-12-02 17:16:43 +0100

    1) minor

--- Stats&Probability/log_semilog_plot.py
+++ Stats&Probability/log_semilog_plot.py
@@ -5,16 +5,31 @@ import seaborn as sns
 
 sns.set_style("whitegrid")
 ax = plt.subplot()
-distr2 = pd.DataFrame(np.random.power(0.1, 1000)).sort(columns=0, ascending=False).reset_index().drop('index', axis=1)
-ax.plot(distr2)
+exponent = 2
+xs = pd.DataFrame(np.arange(1, 1000))
+ys = xs**exponent
+
+ax.scatter(xs, ys, alpha=0.5, c='black')
 ax.set_yscale('log')
+ax.set_xscale("log")
 plt.show()
 
-distr = pd.DataFrame(np.random.standard_cauchy(1000)).sort(columns=0, ascending=False).reset_index().drop('index', axis=1)
+quit()
+
+
+ys.plot()
+plt.show()
+
+distr2 = pd.DataFrame(np.random.power(0.1, 1000)).sort_values(0,
+                                                              ascending=False).reset_index().drop('index', axis=1)
+
+distr = pd.DataFrame(np.random.standard_cauchy(1000)).sort_values(0,
+                                                           ascending=False).reset_index().drop('index', axis=1)
 distr.plot()
 
-simulated_df = distr.sort(columns=0, ascending=False).reset_index().reset_index()
+simulated_df = distr.sort_values(0, ascending=False).reset_index().reset_index()
 plt.show()
+
 simulated_df['freq'] = simulated_df.loc[:, 'level_0'] / len(simulated_df.index) * 100
 simulated_df.plot(kind='scatter', x=0, y='freq')
 ax = plt.subplot()
