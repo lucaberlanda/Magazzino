59589f55a580f2421948259b92c3b842fa25df20
Luca Berlanda <luca.berlanda@euclidea.com>
2019-02-20 17:16:52 +0100

    1) dd distribution

--- Finance/drawdowns_distribution.py
+++ Finance/drawdowns_distribution.py
@@ -1,3 +1,4 @@
+import quandl
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -6,6 +7,7 @@ import matplotlib.pyplot as plt
 def plot_drawdown(dist):
     dist = dist.apply(lambda x: None if x >= 0 else x)
     dist = dist.dropna().reset_index()
+    dist.columns = ['index', 'rets']
     dist['days_passed'] = (dist['index'] - dist['index'].shift()).shift(-1)
 
     dd_groups_list = []
@@ -20,7 +22,7 @@ def plot_drawdown(dist):
 
     dd_dict = {}
     for dd_series, counter in zip(dd_groups_list, range(len(dd_groups_list))):
-        drawdown_rets = [1] + (dd_series.loc[:, 0] + 1).values.tolist()
+        drawdown_rets = [1] + (dd_series.loc[:, 'rets'] + 1).values.tolist()
         drawdown = pd.Series(drawdown_rets).cumprod().values[-1] - 1
         dd_dict[counter] = drawdown
 
@@ -32,8 +34,10 @@ def plot_drawdown(dist):
 
 
 n = 1000
-aaa = pd.Series(np.random.normal(0, 0.01, n))
-aaa = pd.Series(np.random.pareto(1, size=n)).pct_change()
-print(aaa)
-plot_drawdown(aaa)
+ndq = pd.Series(np.random.normal(0, 0.01, n))
+ndq = quandl.get("NASDAQOMX/COMP-NASDAQ", trim_start='1990-03-01', trim_end='2018-04-03').loc[:, 'Index Value']
+ndq_rets = ndq.pct_change().dropna()
+ndq_rets2 = pd.Series(ndq_rets.values, index=ndq_rets.index)
+# aaa = pd.Series(np.random.pareto(1, size=n)).pct_change()
+plot_drawdown(ndq_rets2)
 
