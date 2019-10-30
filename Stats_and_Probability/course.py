8c44d41382c580422a06a86e5cd257c657761f01
BabyBetulla <luca.berlanda@gmail.com>
2019-10-30 08:17:00 +0100

    midterm

--- Stats_and_Probability/course.py
+++ Stats_and_Probability/course.py
@@ -4,21 +4,205 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import scipy.stats as sc
 
+
+l = []
+n2 = 10000
+
+mean1 = 1
+mean2 = 1  # H_0 respected
+sd1 = 2
+sd2 = 3
+
+for i in range(n2):
+    n = 1000
+    gauss1 = pd.Series(np.random.normal(mean1, sd1, n))
+    gauss2 = pd.Series(np.random.normal(mean2, sd2, n))
+
+    diff = gauss1.mean() - gauss2.mean()
+    l.append(diff)
+
+diff_distr = (pd.Series(l) * np.sqrt(n) / np.sqrt(sd1**2 + sd2**2))
+diff_distr.plot(kind='hist', bins=100, alpha=0.5)
+pd.Series(np.random.normal(0, 1, n2)).plot(kind='hist', bins=100, alpha=0.5)
+
+plt.show()
+
+fin = 0.8
+l1 = []
+l2 = []
+for theta in np.arange(0.33, fin, 0.01):
+    tau = 1.0
+    p = theta ** tau
+    as_var = ((theta ** (2 - 2 * tau)) / (tau ** 2)) * (p * (1 - p))
+    l1.append(as_var)
+
+    tau = 2.0
+    p = theta ** tau
+    as_var = ((theta ** (2 - 2 * tau)) / (tau ** 2)) * (p * (1 - p))
+    l2.append(as_var)
+
+pd.Series(l1).plot()
+pd.Series(l2).plot()
+
+d = {}
+for tau in [0.8, 0.9, 1.0, 1.1, 1.2, 1.3, 1.4, 1.5, 1.6, 1.7, 1.8, 1.9, 2.0, 2.1]:
+    as_var = ((theta ** (2 - 2 * tau)) / tau ** 2) * (p * (1 - p))
+    d[tau] = as_var
+
+pd.Series(d).plot()
+plt.show()
+
+# CONFIDENCE INTERVAL
+n = 500
+theta = 0.6
+tau = 2
+p = theta ** tau
+
+l1 = []
+l2 = []
+for _ in range(1000):
+    draws = pd.Series(np.random.binomial(1, p, n))
+    l1.append(draws.mean())
+    l2.append((draws.mean())**(1.0/tau))
+
+# print(pd.Series(l2))
+
+print(pd.Series(l1).var(), (p*(1-p))/n)
+print(pd.Series(l2).var(), (((theta**(2 - 2*tau))/tau**2)*(p*(1-p)))/n)
+
+# CONFIDENCE INTERVAL
+n = 100  # number of samples
+beta = 1/ 0.478035801 # in python, beta = 1/lambda. SO lambda = 2
+
+# in the exercise, lambda = ln(1/theta), hence theta=1/exp(lambda) or
+#                                              theta=1/exp(1/avg(X_i))
+
+l = []
+for _ in range(10000):
+    draws = np.random.exponential(beta, n)
+    l.append(1 / np.exp(1 / draws.mean()))  # theta estimates
+theta_dist = pd.Series(l)
+theta_mean = theta_dist.mean()
+theta_asymptotic_var = (1 / (np.exp(1/beta) ** 2) * (1/beta)**2)  # known
+
+print(theta_dist.var(), theta_asymptotic_var/float(n))
+
+theta_dist.plot(kind='hist', bins=50)
+plt.axvline(x=theta_mean + 1.65 * np.sqrt(theta_asymptotic_var/float(n)), c='red', linewidth=2)
+print(theta_mean + 1.65 * np.sqrt(theta_asymptotic_var/float(n)))
+plt.show()
+
+
+n = 10
+l = []
+for _ in range(10000):
+    a = np.random.exponential(0.5, n)
+    l.append(1 / a.mean())
+
+mean_s = pd.Series(l)
+
+print(mean_s.var(), 4.0 / float(n))
+### Exercise 1a
+n = 100000
+a = pd.Series(np.random.normal(0, 1, n))
+b = pd.Series(np.random.normal(0, 1, n)) ** 5
+c = pd.concat([a, b], axis=1)
+c.columns = ['a', 'b']
+flt_c = c[(c.a < 2) & (c.a > -2) & (c.b < 2) & (c.b > -2)]
+print(float(len(flt_c.index)) / float(n))
+
+### Exercise 1a
+n = 1000
+l = []
+for i in range(n):
+    a = pd.Series(np.random.normal(0, 1, 5))
+    l.append(a.mean())
+
+print(pd.Series(l).var(), 1.0 / float(5))(n)
+
+### Exercise 1c
+n = 100
+l2 = []
+for i in range(1, 10000):
+    l = []
+    for _ in range(n):
+        a = pd.Series(np.random.normal(0, 1, i))
+        l.append(a.mean())
+    l2.append(pd.Series(l).sort_values().iloc[75])
+
+pd.Series(l2).plot()
+plt.show()
+
+result = []
+for i in range(1000):
+    sample1 = np.random.uniform(0, 1, 100000)
+
+    sample2 = []
+    for i in sample1:
+        sample2.append(np.random.uniform(i, i + 1))
+
+    aa = pd.concat([pd.Series(sample1), pd.Series(sample2)], axis=1).cov()
+    result.append(aa.iloc[0, 1])
+
+bb = pd.Series(result)
+bb.plot()
+cc = pd.Series(bb.mean(), index=bb.index)
+cc.plot()
+print(bb.mean())
+plt.show()
+quit()
+n = 100
+sample = np.random.normal(0, 1, n)
+
+aa = []
+for i in range(100):
+    n = 1000000
+    sample = np.random.normal(0, 2, n)
+    aa.append(pd.concat([pd.Series(sample), pd.Series(sample) ** 2], axis=1).cov().iloc[0, 1])
+
+pd.Series(aa).plot()
+
+p = 0.5
+n = 10
+var_l = []
+for _ in range(1000):
+    a = pd.Series(np.random.binomial(1, p, n)).mean()
+    b = a * (1 - a)
+    var_l.append(b)
+
+print(pd.Series(var_l).mean() - p * (1 - p), ((p - p ** 2) / n))
+
+p = 0.5
+
+mean_l = []
+for _ in range(1000):
+    a = pd.Series(np.random.binomial(1, p, 100)).mean() ** 2
+    mean_l.append(a)
+
+c_r = [float(0.5), float(0.75), float(1), float(2)]
+
+for c in c_r:
+    exp_mean = pd.Series(np.random.binomial(1, 0.5, 1000)).expanding().mean() - float(0.5)
+    exp_mean = exp_mean * (1000 ^ float(c))
+    exp_mean.plot()
+    plt.show()
+
+quit()
 # Final Exam Exercise 4
 mean_l = []
 for i in range(1, 10000):
-    a = pd.Series(np.random.uniform(0,1, i)).cumsum()
-    c_n = float(i)/2 + np.sqrt(float(i) / 12)
-    mean_l.append(a.iloc[-1]> c_n)
+    a = pd.Series(np.random.uniform(0, 1, i)).cumsum()
+    c_n = float(i) / 2 + np.sqrt(float(i) / 12)
+    mean_l.append(a.iloc[-1] > c_n)
 
 print(mean_l)
-#print(pd.Series(mean_l).expanding().mean())
+# print(pd.Series(mean_l).expanding().mean())
 pd.Series(mean_l).expanding().mean().plot()
 plt.show()
 quit()
 
 # Final Exam Exercise 2
-size=100000
+size = 100000
 gloves = [1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9, 10, 10]
 l = []
 
@@ -31,7 +215,6 @@ for i in range(size):
 
 print(float(pd.Series(l).sum()) / float(size))
 quit()
-
 
 # Final Exam Exercise 1
 size = 1000000
@@ -52,9 +235,6 @@ aa = pd.Series(np.random.poisson(6, size))
 print(float(len(aa[aa == 9].index)) / size)
 print('ciao')
 
-
-
-
 # Final Exam Exercise 3
 size = 100
 a = np.random.uniform(0, 1, size)
@@ -62,8 +242,6 @@ b = np.random.uniform(0, 1, size)
 ab = pd.DataFrame([a, b]).T
 print(ab.min(axis=1).mean())
 print('ciao')
-
-
 
 ##############
 
@@ -102,16 +280,15 @@ print('ciao')
 quit()
 
 a = pd.Series(np.random.normal(2, 3, 100000))
-b = pd.concat([a * 1/3, a.shift() * 2/3], axis=1).dropna().sum(axis=1).expanding().mean()
+b = pd.concat([a * 1 / 3, a.shift() * 2 / 3], axis=1).dropna().sum(axis=1).expanding().mean()
 b.plot()
 plt.show()
 print(b)
 quit()
 
-
 draws = []
 for n in range(1, 1000):
-    #a = np.random.exponential(1/float(n))
+    # a = np.random.exponential(1/float(n))
     a = np.random.exponential(n)
     print(a)
     draws.append(a)
@@ -133,15 +310,13 @@ plt.show()
 print(bb)
 quit()
 
-
 ## Exercise 2
 h_l = []
 for i in range(100000):
     x = np.random.uniform(0, 1)
     y = np.random.uniform(0, 1)
-    h =(x + 2) * y
+    h = (x + 2) * y
     h_l.append(h)
-
 
 aa = pd.Series(np.log(h_l))
 bb = aa[aa < np.log(2)]
@@ -156,7 +331,6 @@ pd.Series(np.log(h_l)).plot(ax=ax2, kind='hist', bins=100)
 
 plt.show()
 quit()
-
 
 # excercise 6
 
@@ -182,7 +356,6 @@ for i in range(100000):
     small_l.append(small)
     big_l.append(big)
 
-
 # excercise 5
 original_signal_l = []
 signal_l = []
@@ -202,7 +375,6 @@ print(float(len(flt_stuff[flt_stuff.os == 1].index)) / float(len(flt_stuff.index
 
 quit()
 
-
 ## Exercise 2b
 dist = []
 for i in range(100000):
@@ -214,12 +386,9 @@ pd.Series(dist).plot(kind='hist')
 plt.show()
 quit()
 
-
-
-
 pd.Series(small_l).plot(kind='hist', bins=10)
 plt.show()
-aa = pd.concat([pd.Series(small_l),pd.Series(big_l)], axis=1)
+aa = pd.concat([pd.Series(small_l), pd.Series(big_l)], axis=1)
 aa.columns = ['short', 'long']
 (aa.short / aa.long).plot(kind='hist', bins=100)
 plt.show()
@@ -228,18 +397,14 @@ print('ciao')
 print('ciao')
 print('ciao')
 
-
-
 print('ciao')
-
-
 
 ## Exercise 4
 std_n_l = []
 y_l = []
 for i in range(100000):
     std_n = np.random.normal(0, 1)
-    y = np.random.normal(-2*std_n, 1)
+    y = np.random.normal(-2 * std_n, 1)
     std_n_l.append(std_n)
     y_l.append(y)
 
@@ -270,8 +435,8 @@ def exercise_5():
         if aa == 2:
             cnt += 1
             if kk == 3:
-                cnt2 +=1
-            # print(cnt, j)
+                cnt2 += 1
+                # print(cnt, j)
 
     print(float(cnt) / float(j))
     print('---')
@@ -288,7 +453,8 @@ def exercise_5():
         len_c.append(len(set(choices)))
 
     aa = pd.Series(len_c)
-    print(1- (float(len(aa[aa == 3].index) / float(i))))
+    print(1 - (float(len(aa[aa == 3].index) / float(i))))
+
 
 def exercise_2_part_1():
     # EXERCISE 1 part 1
@@ -306,6 +472,7 @@ def exercise_2_part_1():
 
     print(float(cnt) / float(i))
 
+
 def exercise_2_part_2():
     # EXERCISE 1 part 2
     s = list(np.arange(1, 21))
@@ -316,5 +483,6 @@ def exercise_2_part_2():
         cnt += aa.iloc[1] == 7
 
     print(float(cnt) / float(i))
+
 
 exercise_2_part_2()
