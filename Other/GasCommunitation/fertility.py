4a2f556cd4cd9075e9fb371c395d028edd12dea4
babybetulla <luca.berlanda@gmail.com>
2020-09-02 18:53:17 +0200

    gas comm fertility

--- Other/GasCommunitation/fertility.py
+++ Other/GasCommunitation/fertility.py
@@ -17,51 +17,51 @@ questions_dict = {1: 'What is your profession?',
                   3: 'In which setting do you spend most of your professional time?',
                   4: 'How long have you been working in your specialty area?',
                   5: 'How many patients do you treat on average per week?',
-                  6: 'Mechanism of action of new disease modifying drugs',
-                  7: 'Family planning, fertility and pregnancy for people with MS',
-                  8: 'Differential diagnosis of MS',
-                  9: 'Biomarkers for diagnosis, monitoring and individualized treatment',
-                  10: 'Definition and characterization of progressive MS phenotypes',
-                  11: 'Paediatric MS: diagnosis and treatment',
-                  12: 'Patient reported outcomes in MS clinical trials',
-                  13: 'Treating acute MS relapses',
-                  14: 'Treating chronic symptoms of MS',
-                  15: 'Are there any other topics, that have not been mentioned above that you would be interested in learning more about? ',
-                  16: 'Which educational formats do you consider most effective for your Continuing Professional Development? ',
-                  17: 'Where possible, please provide specific examples of educational activities or materials (e.g. title of congress, website, journal, etc.).',
-                  18: 'If you took part in e-learning activities this year, what did you like/dislike about them?',
-                  19: 'Do you use your phone for medical education?',
-                  20: 'Are you seeing patients remotely, using telemedicine?',
-                  21: 'If YES, what do you like like/dislike about it?',
-                  22: 'Which of the following factors most influence your decision making when selecting an educational activity?',
-                  23: 'Please, rate your willingness to travel to meetings in 2021'}
+                  6: 'Endocrine diseases and infertility (including polycystic ovaries)',
+                  7: 'Autoimmune diseases and infertility',
+                  8: 'Endometriosis',
+                  9: 'Fertility preservation (for cancer / benign diseases or elective)',
+                  10: 'Male infertility',
+                  11: 'Ultrasound examination in infertile patients',
+                  12: 'Endometrium and fertility (reproductive implantation failure)',
+                  13: 'Complex cases of ovarian stimulation',
+                  14: 'Progesterone in ART',
+                  15: 'In vitro maturation of oocyte',
+                  16: 'Optimal embryo selection',
+                  17: 'Are there any other topics, that have not been mentioned above that you would be interested in learning more about? ',
+                  18: 'Which educational formats do you consider most effective for your Continuing Professional Development? ',
+                  19: 'Where possible, please provide specific examples of educational activities or materials (e.g. title of congress, website, journal, etc.).',
+                  20: 'If you took part in e-learning activities this year, what did you like/dislike about them?',
+                  21: 'Do you use your phone for medical education?',
+                  22: 'Are you seeing patients remotely, using telemedicine?',
+                  23: 'If YES, what do you like like/dislike about it?',
+                  24: 'Which of the following factors most influence your decision making when selecting an educational activity?',
+                  25: 'Please, rate your willingness to travel to meetings in 2021'}
 
-bow = [17, 18, 21]
-bow_label = {17: 'edu_material',
-             18: 'e_learn_pro_cons',
-             21: 'like_dislike_remote_patients'}
+bow = [19, 20, 23]
+bow_label = {19: 'edu_material',
+             20: 'e_learn_pro_cons',
+             23: 'like_dislike_remote_patients'}
 
-mul_choice = [16, 22]
-mul_choice_label = {16: 'edu_formats',
-                    22: 'selecting_edu_factors'}
+mul_choice = [18, 24]
+mul_choice_label = {18: 'edu_formats',
+                    24: 'selecting_edu_factors'}
 
 filter_for = [1,  # profession
-              2,  # specialty
               3,  # where
               4,  # how long
               5,  # how many
-              30]  # continent
+              32]  # continent
 
 filter_name = {1: 'profession',
-               2: 'specialty',
                3: 'where',
                4: 'how_long',
                5: 'how_many',
-               30: 'continent'}
+               32: 'continent'}
 
-me = ['4uftovws7eos7cjoro274uftov7no9ey']
+me = ['9f6rrrg443w3wehv9f6rr6vhm3o6bi1v']
 
-df = pd.read_excel('neuro.xlsx', header=[0, 1, 2]).T
+df = pd.read_excel('fertility.xlsx', header=[0, 1, 2]).T
 df.columns = df.iloc[0, :].values
 df = df.iloc[1:, :]
 df = df.stack().reset_index()
@@ -93,12 +93,12 @@ for question in mul_choice:
     mul_choice_dict[mul_choice_label[question]] = mul_choice_df
 
 # TO EXCEL
-writer = pd.ExcelWriter('neuro_tableau.xlsx', engine='xlsxwriter')
+writer = pd.ExcelWriter('fertility_tableau.xlsx', engine='xlsxwriter')
 for k in bow_dict.keys():
     bow_dict[k].to_excel(writer, sheet_name=k)
 
 for k in mul_choice_dict.keys():
     mul_choice_dict[k].to_excel(writer, sheet_name=k)
 
-df.to_excel(writer, sheet_name='neuro_tableau')
+df.to_excel(writer, sheet_name='fertility_tableau')
 writer.save()
